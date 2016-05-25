845deafe01590dd0aba50dc676680af7408d5ea9
shaoguangleo <shaoguangleo@gmail.com>
2016-05-25 20:01:16 +0800

    Fix typo error

--- mpi4py/send_recv.py
+++ mpi4py/send_recv.py
@@ -8,7 +8,7 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 # Point2Point communication
-data_send = [comm_rank] * 5
+data_send = [rank] * 5
 comm.send(data_send,dest=(rank+1)%size)
 data_recv = comm.recv(source=(rank-1)%size)
 
