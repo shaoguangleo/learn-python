b2b526ad6125ba00b3b9ad3e2eb9b2cae3e24c87
shaoguangleo <shaoguangleo@gmail.com>
2016-08-01 11:05:18 +0800

    Addin the first simple optparse demo

--- optparse/simple.py
+++ optparse/simple.py
@@ -10,4 +10,7 @@ from optparse import OptionParser
 parser = OptionParser()
 
 parser.add_option('-f','--filename',dest='filename',help='write report to FILE',metavar='FILE')
+parser.add_option('-q','--quiet',action='store_false',dest='verbose',default=True,help='don\'t print status messages to stdout')
+
+(options,args) = parser.parse_args()
 
