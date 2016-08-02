f1f2ec7f3a8d6ca11d7ec27ef60759351cab2ee1
shaoguangleo <shaoguangleo@gmail.com>
2016-08-02 17:40:52 +0800

    Adding split and splitext use case in numpy

---
+++ os/splitext_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Author    :   Guo Shaoguang
+# Email     :   sgguo@shao.ac.cn
+# Institute :   Shanghai Astronomical Observatory
+
+import os
+
+str = '/home/leo/mycode/python/hello.py'
+
+print 'str is :' , str
+
+print 'split(str) : ', os.path.splitext(str)
